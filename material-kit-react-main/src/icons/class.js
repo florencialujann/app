5d2e7162ddac025f6b4550614291026456637765
Florencia Rodriguez <65073910+florencialujann@users.noreply.github.com>
2022-09-30 01:22:17 -0300

    agregando icons pertinentes :D

---
+++ material-kit-react-main/src/icons/class.js
@@ -0,0 +1,16 @@
+import { createSvgIcon } from '@mui/material/utils';
+
+export const Class = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"
+      clipRule="evenodd"
+    />
+  </svg>,
+  'Class'
+);
---
+++ material-kit-react-main/src/icons/home.js
@@ -0,0 +1,17 @@
+import { createSvgIcon } from '@mui/material/utils';
+
+export const Home = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"
+      clipRule="evenodd"
+    />
+  </svg>,
+  'Home'
+);
+
