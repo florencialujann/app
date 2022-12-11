7280b976ad6d06520e43e7bd2a39fa52699ef507
Miguel Mombelli <miguelmombelli@gmail.com>
2022-12-11 20:07:06 -0300

    registrarse 11/12

--- material-kit-react-main/src/components/account/account-profile-details.js
+++ material-kit-react-main/src/components/account/account-profile-details.js
@@ -7,9 +7,13 @@ import {
   CardHeader,
   Divider,
   Grid,
-  TextField
+  TextField,
+  DesktopDatePicker
 } from '@mui/material';
 
+  
+
+  
 const states = [
   {
     value: 'caba',
@@ -32,15 +36,118 @@ export const AccountProfileDetails = (props) => {
     email: 'RominaPerez@Gmail.com',
     phone: '4749',
     state: 'CABA',
-    country: 'Argentina'
+    country: 'Argentina',
+    title: 'titulo',
+    experiencia: 'experiencia',
+    Fecha_Nacimiento: '21/12/1999',
+    Estudios_cursados: 'Estudios_cursados'
   });
-
+  
   const handleChange = (event) => {
     setValues({
       ...values,
       [event.target.name]: event.target.value
     });
   };
+
+
+const Clases = () => {
+    const PerfilUsuario = globalThis.sessionStorage.getItem('rol');
+    if (PerfilUsuario == 'Alumno') {
+      return (
+          <Alumno/>
+      );
+  }
+    else if(PerfilUsuario == 'Profesor') {
+      return (
+          <Profesor/>
+      );
+    }
+  }
+
+const Profesor = () => {
+  return ( 
+    <Grid
+      item
+      xs={12}
+     >
+
+        <Grid
+        Item
+        xs={12}
+        >
+        <TextField
+          fullWidth
+          label="titulo"
+          name="titulo"
+          onChange={handleChange}
+          required
+          value={values.title}
+          variant="outlined"
+        />
+        </Grid>
+        <Grid
+        Item
+        xs={12}
+        >
+        <TextField
+          fullWidth
+          id="Experiencia"
+          label="Experiencia"
+          placeholder="Escriba su experiencia"
+          name="experiencia"
+          onChange={handleChange}
+          required
+          value={values.experiencia}
+          multiline
+          variant="filled"
+        />
+        </Grid>
+    </Grid>
+  );
+}
+
+const Alumno = () => {
+  return (
+    <Grid
+      item
+      xs={12}
+      >
+      <Grid
+      Item
+      xs={12}
+      > 
+      <DesktopDatePicker
+          
+          label="Fecha de Nacimiento"
+          inputFormat="DD/MM/YYYY"
+          //value={values.Fecha_Nacimiento}
+          //onChange={handleChange}
+          required
+          //renderInput={(params) => <TextField {...params} />}
+        />
+      </Grid>
+        <Grid
+          Item
+          s={12}
+        >
+          <TextField
+          fullWidth
+          id="ESTUDIOS_CURSADOS"
+          label="ESTUDIOS CURSADOS"
+          placeholder="Escriba sus estudios cursados"
+          name="Estudios_cursados"
+          onChange={handleChange}
+          required
+          value={values.Estudios_cursados}
+          multiline
+          variant="filled"
+        />
+
+      </Grid>
+    </Grid>
+  );
+}
 
   return (
     <form
@@ -161,6 +268,7 @@ export const AccountProfileDetails = (props) => {
                 ))}
               </TextField>
             </Grid>
+            <Clases />
           </Grid>
         </CardContent>
         <Divider />
